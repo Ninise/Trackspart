88b1c953fe000da8d93dd6fa3785b9dcd2d79275
Ninise <nikitonze@hotmail.com>
2016-04-05 18:05:12 +0300

    App logo added

---
+++ app/src/main/java/com/ninise/trackspart/mvp/view/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ninise.trackspart.mvp.view.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ninise.trackspart.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
