bf75da64596d3549de17e0df0110763bc99ac7ba
Ninise <nikitonze@hotmail.com>
2016-04-06 19:41:41 +0300

    Some changes

--- app/src/main/java/com/ninise/trackspart/mvp/view/main/MainActivity.java
+++ app/src/main/java/com/ninise/trackspart/mvp/view/main/MainActivity.java
@@ -1,7 +1,38 @@
 package com.ninise.trackspart.mvp.view.main;
 
-/**
- * Created by ninise on 06.04.16.
- */
-public class MainActivity {
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.ninise.trackspart.R;
+
+import butterknife.Bind;
+import butterknife.BindDrawable;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.homeToolbar) Toolbar mHomeToolbar;
+    @BindDrawable(R.mipmap.ic_launcher) Drawable mLogoToolbarDrawable;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mHomeToolbar);
+        mHomeToolbar.setNavigationIcon(mLogoToolbarDrawable);
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        ButterKnife.unbind(this);
+        super.onDestroy();
+    }
 }
