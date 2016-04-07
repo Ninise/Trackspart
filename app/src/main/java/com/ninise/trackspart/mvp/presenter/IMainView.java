4033f89ca02e6066ec743680bc8a5bd3219955bd
Ninise <nikitonze@hotmail.com>
2016-04-07 19:12:47 +0300

    Some changes

---
+++ app/src/main/java/com/ninise/trackspart/mvp/presenter/IMainView.java
@@ -0,0 +1,7 @@
+package com.ninise.trackspart.mvp.presenter;
+
+/**
+ * Created by ninise on 07.04.16.
+ */
+public interface IMainView {
+}
--- app/src/main/java/com/ninise/trackspart/mvp/view/main/MainActivity.java
+++ app/src/main/java/com/ninise/trackspart/mvp/view/main/MainActivity.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.ninise.trackspart.R;
 
 import butterknife.Bind;
@@ -15,6 +20,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.homeToolbar) Toolbar mHomeToolbar;
+    @Bind(R.id.homeStartButton) Button mGoButton;
+    @Bind(R.id.homeTabLay) TableLayout mTableLayout;
     @BindDrawable(R.mipmap.ic_launcher) Drawable mLogoToolbarDrawable;
 
     @Override
@@ -27,7 +34,32 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mHomeToolbar);
         mHomeToolbar.setNavigationIcon(mLogoToolbarDrawable);
 
+        TableRow setsRow = (TableRow) mTableLayout.getChildAt(0);
+        TableRow secsRow = (TableRow) mTableLayout.getChildAt(1);
+        TableRow restRow = (TableRow) mTableLayout.getChildAt(2);
 
+        TextView sets = (TextView) setsRow.getChildAt(1);
+        TextView secs = (TextView) secsRow.getChildAt(1);
+        TextView rest = (TextView) restRow.getChildAt(1);
+
+        RxView.clicks(setsRow.getChildAt(2)).subscribe(
+
+        );
+
+        RxView.clicks(setsRow.getChildAt(0)).subscribe(
+
+        );
+
+        RxView.clicks(secsRow.getChildAt(2)).subscribe();
+        RxView.clicks(secsRow.getChildAt(0)).subscribe();
+
+        RxView.clicks(restRow.getChildAt(2)).subscribe();
+        RxView.clicks(restRow.getChildAt(0)).subscribe();
+
+
+        RxView.clicks(mGoButton).subscribe(
+
+        );
     }
 
     @Override
