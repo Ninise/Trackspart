2f9ae55fb5bf565eeedbc946f70032c6978971ad
Ninise <nikitonze@hotmail.com>
2016-04-07 19:56:05 +0300

    Bean for sets added

---
+++ app/src/main/java/com/ninise/trackspart/mvp/model/beans/Sets.java
@@ -0,0 +1,36 @@
+package com.ninise.trackspart.mvp.model.beans;
+
+
+public class Sets {
+
+    private static Sets mInstance = null;
+
+    int count;
+
+    private Sets() {
+        this.count = 0;
+    }
+
+    public static Sets getInstance() {
+        if (mInstance == null) {
+            mInstance = new Sets();
+        }
+        return mInstance;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void incSets() {
+           count += 1;
+    }
+
+    public void decSets() {
+        if (count <= 0) {
+            count = 0;
+        } else {
+            count -= 1;
+        }
+    }
+}
