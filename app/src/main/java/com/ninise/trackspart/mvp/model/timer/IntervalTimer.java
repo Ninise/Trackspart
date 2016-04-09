a1d21a35659c94b64eea4ceb37affb3189d51554
Ninise <nikitonze@hotmail.com>
2016-04-09 19:20:03 +0300

    Constants class  added

--- app/src/main/java/com/ninise/trackspart/mvp/model/timer/IntervalTimer.java
+++ app/src/main/java/com/ninise/trackspart/mvp/model/timer/IntervalTimer.java
@@ -6,10 +6,14 @@ import java.util.concurrent.TimeUnit;
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
-public class SecondsTimer {
+public class IntervalTimer {
 
     public static Observable<Long> startSecondsTimer(int count) {
+//        return Observable.just(Observable.interval(1, TimeUnit.SECONDS, Schedulers.io())
+//                .filter(i -> i <= count)
+//        );
+
         return Observable.interval(1, TimeUnit.SECONDS, Schedulers.io())
-                .filter(i -> i <= count);
+                .take(count);
     }
 }
---
+++ app/src/main/java/com/ninise/trackspart/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.ninise.trackspart.utils;
+
+public class Constants {
+
+    public static final String SETS = "sets";
+    public static final String SECONDS = "seconds";
+    public static final String REST = "rest";
+}
