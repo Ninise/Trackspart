9aaef35797826433e1cf3239fca03e8f93de76d3
Ninise <nikitonze@hotmail.com>
2016-04-11 18:06:45 +0300

    Interface for TimerFragment added

--- app/src/main/java/com/ninise/trackspart/mvp/presenter/main/fragment/ITimerView.java
+++ app/src/main/java/com/ninise/trackspart/mvp/presenter/main/fragment/ITimerView.java
@@ -1,7 +1,11 @@
 package com.ninise.trackspart.mvp.presenter.main.fragment;
 
-/**
- * Created by ninise on 11.04.16.
- */
-public interface ITimerView {
+import com.ninise.trackspart.mvp.presenter.IStateView;
+
+public interface ITimerView extends IStateView {
+
+    void onSecs();
+    void onRest();
+    void onStopTimer();
+
 }
